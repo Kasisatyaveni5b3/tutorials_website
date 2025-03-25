9a3567fc6f732f1624fa44b5ec1704aeef09a077
kasisatyaveni5b3 <kasisatyaveninukella@gmail.com>
2025-03-25 12:26:47 +0530

    added main page for tutorials page

--- tutorials_web_ui/src/app/app-routing.module.ts
+++ tutorials_web_ui/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainPageComponent } from './components/main-page/main-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'mainPage', component: MainPageComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
